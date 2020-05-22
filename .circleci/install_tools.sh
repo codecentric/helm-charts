c44517bfefbbc52ed2f538149cdf76a892662181
Reinhard Nägele <unguiculus@gmail.com>
2020-05-22 17:46:43 +0200

    Revert "Update CI (#232)"
    This reverts commit 4ed8af5ad4cfece7fc8c96c075dcfd5a8c142aeb.

--- .circleci/install_tools.sh
+++ .circleci/install_tools.sh
@@ -2,8 +2,8 @@
 
 set -o errexit
 
-readonly HELM_VERSION=2.16.7
-readonly CHART_RELEASER_VERSION=v1.0.0-beta.1
+readonly HELM_VERSION=2.14.2
+readonly CHART_RELEASER_VERSION=0.2.2
 
 echo "Installing Helm..."
 curl -LO "https://kubernetes-helm.storage.googleapis.com/helm-v$HELM_VERSION-linux-amd64.tar.gz"
